34f3bce6ff9aef39f2bf164b5c4839d9a16814f7
rahilyanz <rahaomurzakova@gmail.com>
2024-07-26 21:13:52 +0600

    Initial Commit

---
+++ SpotifyAppR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyAppR
+//
+//  Created by Rahilya Nazaralieva on 26/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
